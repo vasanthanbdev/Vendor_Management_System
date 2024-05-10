e9330cb0ce1b4dd73bf93efe7ed624ea563fb315
vasanthanbdev <vasanthanbdev@gmail.com>
2024-05-10 11:03:32 +0530

    fields=all

--- api/models.py
+++ api/models.py
@@ -25,13 +25,13 @@ class Vendor(models.Model):
 class PurchaseOrder(models.Model):
     po_number = models.CharField(max_length=20, unique=True)
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    order_date = models.DateTimeField()
-    delivery_date = models.DateTimeField()
+    order_date = models.DateTimeField(auto_now_add=True)
+    delivery_date = models.DateTimeField(null=True)
     items = models.JSONField()
     quantity = models.IntegerField()
     status = models.CharField(max_length=20)
-    quality_rating = models.FloatField(max_length=20)
-    issue_date = models.DateTimeField()
+    quality_rating = models.FloatField(max_length=20, null=True)
+    issue_date = models.DateTimeField(auto_now_add=True)
     acknowledgement_date = models.DateTimeField(null=True)
     
     class Meta:
--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
-from .models import Vendor
+from .models import Vendor, PurchaseOrder, Performance
 
 
 # Vendor Serializer - Using ModelSerializer from the DRF
 class VendorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vendor
-        fields = ['name', 'contact_details', 'address', 'vendor_code']
+        fields = '__all__'
         
+class PurchaseOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PurchaseOrder
+        fields = '__all__'
